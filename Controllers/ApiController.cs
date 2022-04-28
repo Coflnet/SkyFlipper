3f61609e7eb5f37a0b2ba0185330aa2b649c7bd9
Äkwav <xekwavx@gmail.com>
2022-04-28 18:08:27 +0200

    load nbt lookups for based

--- Controllers/ApiController.cs
+++ Controllers/ApiController.cs
@@ -28,10 +28,14 @@ namespace Coflnet.Sky.Flipper.Controllers
         [Route("/flip/{uuid}/based")]
         public async Task<IEnumerable<SaveAuction>> Get(string uuid)
         {
-            var auction = AuctionService.Instance.GetAuction(uuid,
+            var auction = await AuctionService.Instance.GetAuctionAsync(uuid,
                 auctions => auctions
                 .Include(a => a.NbtData)
+                .Include(a=>a.NBTLookup)
                 .Include(a => a.Enchantments));
+            _logger.LogInformation(Newtonsoft.Json.JsonConvert.SerializeObject(auction));
+            if(auction == null)
+                return new List<SaveAuction>();
 
             return (await FlipperEngine.Instance.GetRelevantAuctionsCache(auction, new FindTracking())).references;
 
