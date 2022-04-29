483a127654bb1fa79d23bdfb47991db3ea2e1683
Äkwav <xekwavx@gmail.com>
2022-04-30 00:37:09 +0200

    fix Quick claw

--- FlipperService.cs
+++ FlipperService.cs
@@ -18,7 +18,7 @@ namespace Coflnet.Sky.Flipper
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             var allIds = await itemsApi.ItemsIdsGetAsync();
-            var tags = new string[] { "MINOS_RELIC", "DWARF_TURTLE_SHELMET", "QUICK_CLAW", "PET_ITEM_TIER_BOOST" };
+            var tags = new string[] { "MINOS_RELIC", "DWARF_TURTLE_SHELMET", "QUICK_CLAW", "PET_ITEM_QUICK_CLAW", "PET_ITEM_TIER_BOOST" };
             Flipper.FlipperEngine.Instance.ValuablePetItemIds = allIds.Where(a => tags.Contains(a.Key)).Select(a => (long)a.Value).ToHashSet();
             await Flipper.FlipperEngine.Instance.ProcessPotentialFlipps(stoppingToken);
         }
